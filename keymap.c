eecdcafe621981372c73aec120776bed21be0eb7
joshua.bragge@hotmail.com <joshua.bragge@hotmail.com>
2018-05-24 21:43:38 -0600

    fixed 0 on symbol layer

--- keymap.c
+++ keymap.c
@@ -163,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_MINS,  KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
                 KC_EQL, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_1,   KC_2,     KC_3,     KC_SLASH,  KC_TRNS,
-                         KC_TRNS, KC_DOT,  KC_TRNS,    KC_TRNS,  KC_TRNS,
+                         KC_0, KC_DOT,  KC_TRNS,    KC_TRNS,  KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
